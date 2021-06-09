0b3dc8e45849879fed0a4bc893e8b51d1a6992d4
Neim-Ramazanoglu <mr.ramazanoglu@gmail.com>
2021-06-09 10:59:48 +0300

    odev2son

---
+++ todolist/js/todolist.js
@@ -0,0 +1,56 @@
+function newElement() {
+    var input = document.getElementById("task").value;
+    var ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input));
+    if (input === "") {
+      $(document).ready(function () {
+        $("#liveWarning").toast("show");
+      });
+    } else {
+      ul.appendChild(li);
+      $(document).ready(function () {
+        $("#liveSuccess").toast("show");
+      });
+    }
+    document.getElementById("task").value = "";
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+      };
+    }
+  }
+  var myNodelist = document.getElementsByTagName("LI");
+  var i;
+  for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+  }
+  var close = document.getElementsByClassName("close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+  var list = document.querySelector("ul");
+  list.addEventListener(
+    "click",
+    function (ev) {
+      if (ev.target.tagName === "LI") {
+        ev.target.classList.toggle("checked");
+      }
+    },
+    false
+  );
+  
